7ee5076fa9c322ff33882a1fa0d7a31ec7aeafd0
Maxime Griot <yamashi@live.com>
2019-08-17 11:53:43 +0200

    Load handler implementation

--- Code/ui/src/OverlayLoadHandler.cpp
+++ Code/ui/src/OverlayLoadHandler.cpp
@@ -2,14 +2,22 @@
 
 void OverlayLoadHandler::OnLoadStart(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, TransitionType transition_type)
 {
-    
+    if (frame->IsMain() && transition_type == TT_EXPLICIT)
+    {
+        m_ready = false;
+    }
 }
 
 void OverlayLoadHandler::OnLoadEnd(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, int httpStatusCode)
 {
+    if (frame->IsMain())
+    {
+        m_ready = true;
+    }
 }
 
 bool OverlayLoadHandler::OnLoadError(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame,
     CefLoadHandler::ErrorCode errorCode, const CefString& failedUrl, CefString& errorText)
 {
+    return false;
 }
