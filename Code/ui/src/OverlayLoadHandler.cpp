199fab2d5faac2bf569b2665a140903f115175ff
Maxime Griot <yamashi@live.com>
2019-08-17 11:51:53 +0200

    Missing functions

--- Code/ui/src/OverlayLoadHandler.cpp
+++ Code/ui/src/OverlayLoadHandler.cpp
@@ -4,3 +4,12 @@ void OverlayLoadHandler::OnLoadStart(CefRefPtr<CefBrowser> browser, CefRefPtr<Ce
 {
     
 }
+
+void OverlayLoadHandler::OnLoadEnd(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, int httpStatusCode)
+{
+}
+
+bool OverlayLoadHandler::OnLoadError(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame,
+    CefLoadHandler::ErrorCode errorCode, const CefString& failedUrl, CefString& errorText)
+{
+}
